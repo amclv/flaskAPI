dcfeab87ee5863c6b0a785a650d601f77ea246ee
Aaron Cleveland <toscleveland@gmail.com>
2021-01-23 02:43:09 -0600

    Cleaned up code

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-api = Api(app)
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-videos = {}
+    # For debugging
+    print(f"got name {name}")
 
-class Video(Resource):
-    def get(self, video_id):
-        return videos[video_id]
+    response = {}
 
-    def put(self, video_id):
-        args = video_put_args.parse_args()
-        return {video_id: args}
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
-api.add_resource(Video, "/video/<int:video_id>")
+    # Return the response in json format
+    return jsonify(response)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
 
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
